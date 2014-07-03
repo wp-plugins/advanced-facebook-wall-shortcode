366db78011cc338b693db41a369ba7930d3d88bf
connexapps <connexapps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-03 08:53:51 +0000

    stable release
    git-svn-id: https://plugins.svn.wordpress.org/advanced-facebook-wall-shortcode/trunk@942692 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ advanced_facebook_wall_shortcode.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * @package advanced-facebook-wall-shortcode
+*/
+/*
+Plugin Name: Advanced Facebook Wall Shortcode
+Plugin URI: http://www.connexapps.com
+Description: Advanced Facebook Wall - Gives you totally customizable awesome facebook wall. Now get your customize facebook result on your wordpress site.
+Version: 0.1
+Author: Ted Lowe
+Author URI: http://www.connexapps.com
+*/
+// Register style sheet.
+add_action( 'wp_enqueue_scripts', 'register_advanced_facebook_wall_shortcode_styles' );
+add_shortcode('ca_facebook', 'advancedFacebookWallShortcode');
+ function advancedFacebookWallShortcode($atts){
+ 	$atts = shortcode_atts(array(
+ 		'facebook_id' => 'smashmag',
+ 		'width' => '400',
+ 		'post_limit' => '5',
+		'link_target' => '_blank',
+		'display_attachment' => 'true',
+ 		'app_id' => '1438026419800246',
+		'app_secret' => '78f65b8644632e0c2d98e053ed39668f'
+ 	), $atts);
+ 	extract($atts);
+        /* Decode - Encode of the URLs Facebook Graph */
+        $graphUser = "https://graph.facebook.com/$facebook_id/?fields=name,picture&access_token=$app_id|$app_secret";
+        $graphPosts = "https://graph.facebook.com/$facebook_id/posts/?access_token=$app_id|$app_secret&date_format=U&limit=$post_limit";
+        $graphUserFeedsData = getDataObject($graphUser);
+        $graphPostsFeedsData = getDataObject($graphPosts);
+        ?>
+<div id="advanced_fb" class="advanced_facebook_wall" style="width: <?php echo $width; ?>px;">
+    <h1><a href="https://www.facebook.com/profile.php?id=<?php echo $graphUserFeedsData->id; ?>" target="<?php echo $link_target; ?>"><?php echo $graphUserFeedsData->name; ?></a><span>on Facebook</span></h1>
+    <div class="adv_fb_post_area">
+        <ul>
+            <?php
+               foreach($graphPostsFeedsData->data as $d):
+            ?>
+            <li>
+                <a href="https://www.facebook.com/profile.php?id=<?php echo $graphUserFeedsData->id; ?>" target="<?php echo $link_target; ?>">
+                    <img src="<?php echo $graphUserFeedsData->picture->data->url; ?>" class="adv_fb_post_img"/>
+                </a>
+                <div class="adv_status">
+                    <h3 class="adv_status_title">
+                        <a href="https://www.facebook.com/profile.php?id=<?php echo $graphUserFeedsData->id; ?>" target="<?php echo $link_target; ?>">
+                            <?php echo $d->from->name; ?>
+                        </a>
+                    </h3>
+                    <p class="adv_status_message">
+                        <?php
+                        //echo $d->type;
+                        if($d->type == "status"){
+                            if(isset($d->story))
+                            echo $d->story;
+                        }else{
+                            if(isset($d->message))
+                                echo addLink($d->message);
+                        }
+                        ?>
+                    </p>
+                    <?php if($display_attachment == "true"): ?>
+                    <?php if($d->type != "status" || isset($d->picture)): ?>
+                    <div class="adv_status_attachment">
+                        <?php
+                            echo "<img src='" . $d->picture . "' class='adv_status_attachment_image'/>";
+                        ?>
+                        <?php if($d->type == "link"): ?>
+                        <div class="adv_status_attachment_data">
+                            <?php if(isset($d->link) && isset($d->name)): ?>
+                            <p class="adv_status_attachment_data_name">
+                                <a href="<?php echo $d->link; ?>" target="_blank">
+                                    <?php echo $d->name; ?>
+                                </a>
+                            </p>
+                            <?php endif; ?>
+                            <?php if(isset($d->caption)): ?>
+                            <p class="adv_status_attachement_data_caption">
+                                <?php echo $d->caption; ?>
+                            </p>
+                            <?php endif; ?>
+                            <?php if(isset($d->description)): ?>
+                            <p class="adv_status_attachement_data_description">
+                                <?php echo $d->description; ?>
+                            </p>
+                            <?php endif; ?>
+                        </div>
+                        <?php endif; ?>
+                    </div>
+                    <?php endif; ?>
+                    <?php endif; ?>
+                    <div class="adv_fb_date">Posted - <?php echo timeAgo($d->created_time);?> ago</div>
+                </div>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
+<?php
+        return '';
+ }
+ /*
+     * Some Function Implementation - For need as the way we go.........
+     */
+    function getDataObject($url){
+        $getData = file_get_contents($url);
+        $getDataJson = json_decode($getData);
+        return $getDataJson;
+    }
+    function addLink($string)
+	{
+		$pattern = '/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/i';
+		$replacement = '<a class="fb_url" href="$1" target="_blank">$1</a>';
+		$string = preg_replace($pattern, $replacement, $string);
+		return $string;
+	}
+    function timeAgo($timestamp){
+            $time = time() - $timestamp;
+            if ($time < 60)
+                return  ( $time > 1 ) ? $time . ' seconds' : 'a second';
+            elseif ($time < 3600) {
+                $tmp = floor($time / 60);
+                return ($tmp > 1) ? $tmp . ' minutes' : ' a minute';
+            }
+            elseif ($time < 86400) {
+                $tmp = floor($time / 3600);
+                return ($tmp > 1) ? $tmp . ' hours' : ' a hour';
+            }
+            elseif ($time < 2592000) {
+                $tmp = floor($time / 86400);
+                return ($tmp > 1) ? $tmp . ' days' : ' a day';
+            }
+            elseif ($time < 946080000) {
+                $tmp = floor($time / 2592000);
+                return ($tmp > 1) ? $tmp . ' months' : ' a month';
+            }
+            else {
+                $tmp = floor($time / 946080000);
+                return ($tmp > 1) ? $tmp . ' years' : ' a year';
+            }
+            }
+ /**
+  * Register style sheet.
+  */
+ function register_advanced_facebook_wall_shortcode_styles() {
+ 	wp_register_style( 'advancedFacebookWallShortcode', plugins_url( 'style.css' , __FILE__ ) );
+ 	wp_enqueue_style( 'advancedFacebookWallShortcode' );
+ }
---
+++ readme.txt
@@ -0,0 +1,103 @@
+=== Advanced Facebook Wall Shortcode ===
+
+Plugin URI: http://www.connexapps.com
+Author Name : Ted Lowe
+Author URL : http://www.connexapps.com
+Tags: advanced facebook, facebook wall, custom facebook wall, facebook wall shortcode, wordpress, facebook wall for wordpress, custom facebook wordpress, wordpress plugin, facebook wordpress shortcode, facebook wordpress plugin, facebook, facebook post feed, facebook feed, facebook feed shortcode, facebook feed for wordpress
+Requires at least: 3.0.1
+Tested up to: 3.9.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Advanced Facebook Wall Shortcode - Displays feeds from Facebook Page Only. (It won't work with facebook group)
+
+== Description ==
+Advanced Facebook Wall Shortcode is a Wordpress Shortcode Plugin which displays Facebook Latest Post Feeds from any Facebook Fan Page.
+
+This use Facebook Opengraph API so its highly customizable and you can style the feeds the way you like.
+
+We have 24/7 Active Support so you can let us know if you are having any kind of issue with configuring our extension.
+
+DEMO - http://www.connexapps.com/demo/advanced-facebook-wall-shortcode/
+
+== Installation ==
+
+Thanks for installing our Wordpress Plugin. In this article we will describe how you can use our PLUGIN perfectly.
+
+We are open with suggestions & questions. So for anything please email us at - admin@connexapps.com
+
+After Installing/ Uploading the Plugin First you have to do is Enabling the Plugin.
+
+Now You can display our shortcode on any place of your wordpress site using our shortcode.
+
+Our Shortcode : [ca_facebook]
+
+This will display the facebook feed with all default values. To change the default values/ Attributes in wordpress you have to set your own values.
+
+1. Facebook ID: It can be your Short URL of facebook page like
+
+Example: https://www.facebook.com/smashmag
+
+Here Facebook ID is - smashmag
+
+If your Facebook Page URL is long then you can find your Facebook page Numeric ID.
+
+go here - http://findmyfacebookid.com/
+
+Submit your Page URL. Then you will find a numeric ID. Copy it; This is your Facebook Unique Id
+
+So to put that on shortcode follow this -:
+
+[ca_facebook facebook_id="YOUR_FACEBOOK_UNIQUE_ID"]
+
+2. Facebook APP ID & Secret:
+
+You have to create account in Facebook Developers site to obtain the keys. You can use the default values, But to its always better you use your own keys.
+
+First Make sure you are login to Facebook
+
+https://developers.facebook.com/
+
+go here and create new app
+
+The a window will popup
+
+Give it a name and choose category. And then click "Create App" Button at bottom.
+
+If Captcha Require. Fill it.
+
+That's it. And you will find your APP Id & APP Secret
+
+In default it will work with default APP ID and APP Secret values from facebook but if you like to put yours then
+
+[ca_facebook facebook_id="samsung" app_id="1438026419800246" app_secret="78f65b8644632e0c2d98e053ed39668f"]
+
+3. Width: Width of your Module in pixel.
+
+[ca_facebook facebook_id="samsung" width="YOUR_PREFERRED_NUMERIC_VALUE"]
+
+4. Post Limit: Limit of the numbers of Post you like to display.
+[ca_facebook facebook_id="samsung" post_limit="5"]
+
+5. Link Target Attributes: Link Target - Open in New Window or Same Window or Top Window
+[ca_facebook facebook_id="samsung" post_link_target="_blank"]
+Can put - "_self" - to load in same page.
+More values from here:
+http://www.w3schools.com/tags/att_a_target.asp
+
+6. Display Attachment: Yes or No.
+
+True - Yes / False - No
+[ca_facebook facebook_id="samsung" display_attachment="true"]
+
+== Frequently Asked Questions ==
+ Contact us for any kind of support - admin@connexapps.com
+
+== Screenshots ==
+
+1. SCREENSHOT
+
+== Changelog ==
+
+= 0.1 =
+Stable version release
