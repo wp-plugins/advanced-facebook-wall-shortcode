990fcee59a25baa369fdd94da06a2f8c102348c9
connexapps <connexapps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-25 00:24:40 +0000

    wordpress shortcode up
    git-svn-id: https://plugins.svn.wordpress.org/advanced-facebook-wall-shortcode/trunk@1032212 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advanced_facebook_wall_shortcode.php
+++ advanced_facebook_wall_shortcode.php
@@ -96,7 +96,7 @@ add_shortcode('ca_facebook', 'advancedFacebookWallShortcode');
         </ul>
     </div>
 <!-- text goes here -->
-<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://www.telemedicine-jobs.com' title='click here' target='_blank'>Telemedicine Jobs</a></div>
+<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://www.telemedicine-jobs.com' title='click here' target='_blank'>telemedicine job opportunities</a></div>
 </div>
 <?php
         return '';
