3c27c3b49aa8b17fb331104fb287efa70d338914
connexapps <connexapps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-29 19:13:29 +0000

    change url in php
    git-svn-id: https://plugins.svn.wordpress.org/advanced-facebook-wall-shortcode/trunk@1189797 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advanced_facebook_wall_shortcode.php
+++ advanced_facebook_wall_shortcode.php
@@ -96,7 +96,7 @@ add_shortcode('ca_facebook', 'advancedFacebookWallShortcode');
         </ul>
     </div>
 <!-- text goes here -->
-<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://crescentappraisal.com/' title='click here' target='_blank'>Seattle Commercial Appraiser</a></div>
+<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://moltzconstructors.com/' title='Moltz' target='_blank'>Moltz Constructors</a></div>
 </div>
 <?php
         return '';
